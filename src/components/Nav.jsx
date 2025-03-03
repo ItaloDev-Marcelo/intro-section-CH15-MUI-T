f0afa4e783902f06052c302689de04a665453305
Italo Marcelo <devfrontend2028study@gmail.com>
2025-03-03 15:07:16 -0300

    up

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,24 @@
-import { Stack, AppBar, Toolbar, Button, IconButton, Drawer, Box,   } from '@mui/material';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import { Stack, AppBar, Toolbar, Button, IconButton, Drawer, Box, List, ListItem, ListItemText, ListItemIcon,   } from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
 import MenuIcon from '@mui/icons-material/Menu'
 import logo from '../assets/images/logo.svg'
 import { useState } from 'react';
 import CloseIcon  from '@mui/icons-material/Close';  
-import BasicMenu from './Drop';
+import Calender from '../assets/images/icon-calendar.svg';
+import Todo from '../assets/images/icon-todo.svg';
+import Planning from '../assets/images/icon-planning.svg';
+import Reminders from '../assets/images/icon-reminders.svg';
+
 
 export default function Nav() {
 
   const [open, setOpen] = useState(false);
+
+  const [drop1, setDrop1]  = useState(false)
+
+  const [drop2, setDrop2]  = useState(false)
+
 
   
 
@@ -30,8 +36,59 @@ export default function Nav() {
         </IconButton>
       </Box>
        
+      <Stack sx={{marginTop: '5em'}} >
+      <Box>
+         <Button className='pop' onClick={() => setDrop1(!drop1)} endIcon={drop1 ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon/>}>Features  </Button>
+         <List style={{display: drop1? 'block' : 'none'}} >
+            <ListItem>
+               <img src={Todo} alt="" />
+                  <ListItemText>Todo List</ListItemText>
+            </ListItem>
+            <ListItem>
+            <img src={Calender} alt="" />
+                  <ListItemText>Calendar</ListItemText>
+            </ListItem>
+            <ListItem>
+            <img src={Reminders} alt="" />
+                  <ListItemText>Reminders</ListItemText>
+            </ListItem>
+            <ListItem>
+            <img src={Planning} alt="" />
+                  <ListItemText>Planning</ListItemText>
+            </ListItem>
+         </List>
+      </Box>
 
+      <Box>
+         <Button className='pop' onClick={() => setDrop2(!drop2)}  endIcon={drop1 ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon/>}>Company  </Button>
+         <List style={{display: drop2? 'block' : 'none'}}>
+            <ListItem>
+                  <ListItemText>History</ListItemText>
+            </ListItem>
+            <ListItem>
+                  <ListItemText>Our Team</ListItemText>
+            </ListItem>
+            <ListItem>
+                  <ListItemText>Blog</ListItemText>
+            </ListItem>
+           
+         </List>
+      </Box>
       
+      <List >
+         <ListItem>
+            <ListItemText>Carrers</ListItemText>
+         </ListItem>
+         <ListItem>
+            <ListItemText>About</ListItemText>
+         </ListItem>
+      </List>
+
+       <Stack  direction='column'>
+       <Button variant='text' sx={{marginBottom: '1em '}} >Login</Button>
+       <Button  variant='outlined' sx={{borderRadius: '25px'}}>Register</Button>
+       </Stack>
+      </Stack>
   
   
     </Box>
